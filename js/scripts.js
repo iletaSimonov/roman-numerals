268b4dd870bd44cf2c6b956c2fece1a8bb1fcd00
Ileta Simonov <ileta.simonov@yahoo.com>
2023-02-08 19:00:35 -0800

    adds key for roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 function toRoman(num) {
-  if (isNaN(num)) {
+  if (isNaN(num)) 
     return NaN;
+  let digits = String(+num).split(""),
+  key = 
+    ["",'C', 'CC', 'CCC', 'CD','D','DC','DCC','DCC','CM',
+    '','X','XX','XXX','XL', 'L', 'LX', 'LXX','LXXX','XC',
+    '','I','II','III','IV','V','VI','VII','VIII','IX'],
+  roman = "",
+  i=3;
   }
-}
